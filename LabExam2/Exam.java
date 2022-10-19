57d4bbf6ff3ba47fa978f4bdee11770618681b74
AbdullahFaisal1 <1398abodi@gmail.com>
2022-10-19 16:37:39 +0300

    exam

---
+++ LabExam2/Exam.java
@@ -0,0 +1,52 @@
+
+import java.util.Scanner;
+
+public class Exam {
+
+    ///// Write your Method Here /////
+    public static void reverseProuctType(LinkedQueue<Character> reversedQ) {
+        if (reversedQ.isEmpty())
+            System.out.println("your Queue is Empty !");
+        else {
+            int numberOfLetters = 0;
+            for (int i = 0; i < reversedQ.length(); i++) {
+                Character val = reversedQ.dequeue();
+                reversedQ.enqueue(val);
+
+                if (val == '-' || Character.isDigit(val))
+                    continue;
+                else
+                    numberOfLetters++;
+            }
+            LinkedStack<Character> stack = new LinkedStack<>();
+            for (int i = 0; i < numberOfLetters; i++) {
+                Character Qvalue = reversedQ.dequeue();
+                stack.push(Qvalue);
+            }
+            while (!stack.isEmpty()) {
+                Character Svalue = stack.pop();
+                reversedQ.enqueue(Svalue);
+            }
+            for (int i = 0; i < reversedQ.length() - numberOfLetters; i++) {
+                reversedQ.enqueue(reversedQ.dequeue());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the product code before correcting : ");
+        String x = input.next();
+
+        // storing the product code to the Queue
+        LinkedQueue<Character> ProductCode = new LinkedQueue<Character>();
+        for (int i = 0; i < x.length(); i++)
+            ProductCode.enqueue(x.charAt(i));
+
+        ///// Complete the code /////
+        System.out.println("\nthe product code after correcting:  ");
+        reverseProuctType(ProductCode); // Koob-1000234 //licnep-1000972
+        ProductCode.print();
+    }
+
+}
---
+++ LabExam2/LinkedStack.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+public class LinkedStack<E> {
+
+    private Node<E> top;
+    private int size;
+
+    public LinkedStack() {
+        top = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void push(E data) {
+        // Make a new node whose next reference is the existing list
+        Node<E> newNode = new Node<>(data, top);
+        top = newNode; // top points to new node
+        size++;
+    }
+
+    public E pop() {
+        if (top == null)
+            return null;
+
+        E t = top.data; // Save the value at the top of the stack
+        top = top.next; // Make top reference points to the next node in the stack
+        size--;
+
+        return t; // return the removed value
+    }
+
+    public E top() {
+        if (top == null)
+            return null;
+
+        return top.data;
+    }
+
+    public void print() {
+        if (size == 0) {
+            System.out.println("Queue is empty");
+
+        }
+        for (int i = 0; i < size; i++) {
+            System.out.print(top.data);
+            System.out.print(" ");
+            push(pop());
+        }
+        System.out.println("");
+
+    }
+
+}
